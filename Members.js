17ae6c6e2851a4c46f5d51d30ddadcdba9070401
AliDemircix <alidemircix@gmail.com>
2022-02-27 21:47:45 +0100

    Express.Js Creating member app finished

---
+++ Members.js
@@ -0,0 +1,33 @@
+const members = [
+  {
+    id: 1,
+    name: 'Ali',
+    email: 'alidemircix@gmail.com',
+    status: 'active',
+  },
+  {
+    id: 2,
+    name: 'Esra',
+    email: 'esrademircix@gmail.com',
+    status: 'active',
+  },
+  {
+    id: 3,
+    name: 'Efe',
+    email: 'efedemircix@gmail.com',
+    status: 'inactive',
+  },
+  {
+    id: 4,
+    name: 'Sami',
+    email: 'samidemircix@gmail.com',
+    status: 'inactive',
+  },
+  {
+    id: 5,
+    name: 'Goktug',
+    email: 'goktugdemircix@gmail.com',
+    status: 'active',
+  },
+];
+module.exports = members;
